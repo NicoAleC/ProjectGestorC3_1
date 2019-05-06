2821256d5ad3e00822052d0b6ff2f4aa2dd8100d
Mateo <mateopuna98@gmail.com>
2019-05-06 19:50:31 -0400

    creacion objeto cuenta y lista de cuentas

---
+++ src/components/Cuenta.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :class= "cuenta">
+    <form action="">
+      <input id = "inputNombre" type="text" name = "nombreCuenta" :value = nombre>
+    </form>
+    <button id = "btnEditar" type = "button" v-on:click = "editarNombre">{{nombreBoton}}</button>
+    <button id = "btnEliminar" type = "button">Eliminar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cuenta',
+  props: ['nombre','ingresos','egresos'],
+  data() {
+    return{
+    nombreBoton: "Guardar"
+    }  
+  },
+
+  methods:{
+
+    editarNombre: function(){
+
+      if(this.nombreBoton == "Editar"){
+        document.getElementById("inputNombre").disabled = false//Habilitar la edicion en el input
+        this.nombreBoton = "Guardar"//Cambiar el nombre del boton
+      }else{
+        this.nombre = document.getElementById("inputNombre").value//Cambiar el nombre de la cuenta
+        document.getElementById("inputNombre").disabled = true//Deshabilitar la edicion en el input
+        this.nombreBoton = "Editar"//Cambiar el nombre del boton
+
+      }     
+    },
+
+    eliminarCuenta: function(){
+      "TODO"
+    }
+    
+  }
+ 
+}
+</script>
+
+<style scoped>
+  
+  #inputNombreC{  
+    float: left;
+    clear: left
+  }
+
+  #btnEliminar{
+    float: right;
+    clear:right
+  }
+</style>
---
+++ src/components/ListaCuentas.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="listaCuentas">
+      <Cuenta
+        id= "cuenta.id"
+        v-for="cuenta in cuentas"
+        :key="cuenta.id"
+      ></Cuenta>
+    <button @click="anadirCuenta">Crear Cuenta</button>
+  </div>
+
+</template>
+
+<script>
+import Cuenta from '@/components/Cuenta.vue'
+
+export default {
+  name: 'ListaCuentas',
+
+  components:{
+    Cuenta: Cuenta
+  },
+ 
+  computed: {
+    cuentas(){
+      return this.$store.state.CUENTAS
+    }
+  },
+
+  methods:{
+    anadirCuenta(){
+      var nuevaCuenta = {id: 0,nombre: 'Cuenta ' + id,transacciones: {}}
+      this.$store.dispatch('addAccounts', nuevaCuenta)
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+</style>
