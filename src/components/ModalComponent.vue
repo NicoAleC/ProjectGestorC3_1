5e393a5389958efddbf41224421646d9a1a85686
Alan Jimenez Arandia <alanjimenez@Alans-MacBook-Pro.local>
2019-05-08 20:55:43 -0400

    Pop up register income expense

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,36 @@
+<template>
+   <div>
+     <transition name="modal">
+      <div v-if="isOpen">
+         
+          <p>
+            <label for="Name">Name:</label>
+            <input id="Name" v-model="name" placeholder="Name">
+          </p>
+
+          <p>
+            <label for="Category">Category:</label>
+            <input id="Category" v-model="category" placeholder="Category">
+          </p>
+
+          <p>
+            <label for="Ammount">Ammount:</label>
+            <input id="Ammount" v-model="ammount" placeholder="Ammount">
+          </p>
+
+      </div>
+     </transition>
+      <button @click="isOpen=!isOpen">Register Income/Expense</button>
+   </div>
+</template>
+
+
+<script>
+   export default{
+       data:function(){
+          return {
+      isOpen: false
+    }
+       }
+   }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <Home msg="Welcome to Your Vue.js App"/>
+    <ModalComponent/>
+    <!--<IncomeExpense/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Home from '@/components/Home.vue'
+import ModalComponent from '@/components/ModalComponent.vue'
 
 export default {
   name: 'home',
   components: {
-    Home
+    Home,
+    ModalComponent
   }
 }
 </script>
