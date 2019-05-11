20f6b6c4b58d298c956c2f118546adaaf1ea7227
Alan Jimenez Arandia <alanjimenez@Alans-MacBook-Pro.local>
2019-05-11 15:30:38 -0400

    Fixing errors

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
--- src/store.js
+++ src/store.js
@@ -5,15 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    CUENTAS: [{id: 11, nombre: 'Mateo', 
-              ingresos: [{descripcion: 'mensualidad abril',monto: 1500, fecha: '12/12/12', categoria: 'Transferencia'}]
-             ,egresos: [{descripcion: 'pago universidad', monto: 300, fecha: '12/12/19',categoria:'Expensas'}]}],
-    CATEGORIAS_INGRESOS: [{id:0, nombre: 'Salario'},
-                          {id:1, nombre: 'Transferencia'},
-                          {id:2, nombre: 'Otros'}],
-    CATEGORIAS_EGRESOS:[{id:0, nombre: 'Expensas'},
-                        {id:1, nombre: 'Transferencia'},
-                        {id:2, nombre: 'Otros'}],
+    CUENTAS: [
+      { id: 11,
+        nombre: 'Mateo',
+        ingresos: [
+          { descripcion: 'mensualidad abril', monto: 1500, fecha: '12/12/12', categoria: 'Transferencia' }
+        ],
+        egresos: [
+          { descripcion: 'pago universidad', monto: 300, fecha: '12/12/19', categoria: 'Expensas' }
+        ]
+      }],
+    CATEGORIAS_INGRESOS: [
+      { id: 0, nombre: 'Salario' },
+      { id: 1, nombre: 'Transferencia' },
+      { id: 2, nombre: 'Otros' }],
+    CATEGORIAS_EGRESOS: [
+      { id: 0, nombre: 'Expensas' },
+      { id: 1, nombre: 'Transferencia' },
+      { id: 2, nombre: 'Otros' }],
     CUENTA_ACTUAL: '',
     TIPO_TRANSACCION: 'Ingresos'
   },
@@ -21,16 +30,16 @@ export default new Vuex.Store({
     addAccount (context, nuevaCuenta) {
       context.CUENTAS.push(nuevaCuenta)
     },
-    addCategoryIncome (context, newCategory){
+    addCategoryIncome (context, newCategory) {
       context.CATEGORIAS_INGRESOS.push(newCategory)
     },
-    addCategoryExpense (context, newCategory){
+    addCategoryExpense (context, newCategory) {
       context.CATEGORIAS_EGRESOS.push(newCategory)
     },
-    changeCurrentAccount(context,nuevoId){
+    changeCurrentAccount (context, nuevoId) {
       context.CUENTA_ACTUAL = nuevoId
     },
-    changeTransaction(context,transaccion){
+    changeTransaction (context, transaccion) {
       context.TIPO_TRANSACCION = transaccion
     }
   },
@@ -38,17 +47,17 @@ export default new Vuex.Store({
     addAccount (context, nuevaCuenta) {
       context.commit('addAccount', nuevaCuenta)
     },
-    addCategoryIncome(context,newCategory){
-      context.commit('addCategoryIncome',newCategory)
+    addCategoryIncome (context, newCategory) {
+      context.commit('addCategoryIncome', newCategory)
     },
-    addCategoryExpense(context,newCategory){
-      context.commit('addCategoryExpense',newCategory)
+    addCategoryExpense (context, newCategory) {
+      context.commit('addCategoryExpense', newCategory)
     },
-    changeCurrentAccount(context,nuevoId){
-      context.commit('changeCurrentAccount',nuevoId)
+    changeCurrentAccount (context, nuevoId) {
+      context.commit('changeCurrentAccount', nuevoId)
     },
-    changeTransaction(context,transaccion){
-      context.commit('changeTransaction',transaccion)
+    changeTransaction (context, transaccion) {
+      context.commit('changeTransaction', transaccion)
     }
   }
 })
