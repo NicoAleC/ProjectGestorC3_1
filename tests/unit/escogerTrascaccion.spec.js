7f2f41f12a49c24c5b9b5189cf76d5341a614ef1
Nicolas Carrasco <chincholax@gmail.com>
2019-05-14 15:58:46 -0400

    Corregidos los errores de Lint

--- tests/unit/escogerTrascaccion.spec.js
+++ tests/unit/escogerTrascaccion.spec.js
@@ -4,35 +4,35 @@ import StoreUtil from './StoreUtil'
 import EscogerTransaccion from '@/components/EscogerTransaccion.vue'
 import Vue from 'vue'
 import Vuex from 'vuex'
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 describe('EscogerTransaccion.vue', () => {
-    let wrapper;
-    let store;
+  let wrapper
+  let store
 
-    before(function () {
+  before(function () {
 
-    })
+  })
 
-    beforeEach(function () {
-        store = StoreUtil.getDefaultStore();
-        wrapper = shallowMount(EscogerTransaccion,
-            {
-                store
-            })
-    })
+  beforeEach(function () {
+    store = StoreUtil.getDefaultStore()
+    wrapper = shallowMount(EscogerTransaccion,
+      {
+        store
+      })
+  })
 
-    it('cambio de estado a Ingresos', () => {
-        wrapper.vm.irAIngresos()
-        expect(wrapper.vm.tipoTransaccion).to.equals('Ingresos')
-    })
+  it('cambio de estado a Ingresos', () => {
+    wrapper.vm.irAIngresos()
+    expect(wrapper.vm.tipoTransaccion).to.equals('Ingresos')
+  })
 
-    it('cambio de estado a Egresos', () => {
-        wrapper.vm.irAEgresos()
-        expect(wrapper.vm.tipoTransaccion).to.equals('Egresos')
-    })
+  it('cambio de estado a Egresos', () => {
+    wrapper.vm.irAEgresos()
+    expect(wrapper.vm.tipoTransaccion).to.equals('Egresos')
+  })
 
-    afterEach(function () {
+  afterEach(function () {
 
-    })
+  })
 })
