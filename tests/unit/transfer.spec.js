3aefd7e5feabb484ad47fd6132aae33b422b9ff0
Ignacio <iggy.b.q@gmail.com>
2019-05-14 17:55:04 -0400

    unit final

--- tests/unit/transfer.spec.js
+++ tests/unit/transfer.spec.js
@@ -1,19 +1,26 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import Transfer from '@/components/Transfer.vue'
+import StoreUtil from './StoreUtil'
+import Transfer from '@/components/DataTable.vue'
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
 
 describe('Transfer.vue', () => {
-  const msg = 'new message'
-  let wrapper
-  beforeEach(function () {
-    wrapper = shallowMount(Transfer, {
-      propsData: { msg }
+  let store = StoreUtil.getDefaultStore()
+
+  it('verifica no se excede la transferencia', () => {
+    let wrapper = shallowMount(Transfer, {
+      store
     })
+    var cuentaNueva = { id: 1000, nombre: 'Prueba', ingresos: [], egresos: [] }
+    wrapper.vm.$store.dispatch('anadirCuenta', cuentaNueva)
+    let cuentasStore = wrapper.vm.cuentas
+    let idCuenta2 = 1000
+    wrapper.vm.amount = -100
+    wrapper.vm.selectedaccount = idCuenta2
+    let egresosC1Antes = JSON.parse(JSON.stringify(cuentasStore[0].egresos))
+   /* let ingresosC2Antes = JSON.parse(JSON.stringify(cuentasStore[1].ingresos))
+    expect(wrapper.vm.saveTransfer()).to.alert()*/
   })
-  /* it('render button', () => {
-    expect(wrapper.find('.button').exists())
-  })
-  it('input slot', () => {
-    expect(wrapper.find('.input').exists())
-  }) */
 })
