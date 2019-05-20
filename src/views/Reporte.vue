71169888f556089675dc29be906c9213958ab0f2
Nicolas Carrasco <chincholax@gmail.com>
2019-05-20 16:29:49 -0400

    router para la vista de reportes

---
+++ src/views/Reporte.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Esta es la vista de reporte</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'reporte',
+    components: {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
