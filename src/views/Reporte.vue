833452b8a9bd610e6a477e5b9422bac857b41d1e
Alan Jimenez Arandia <aljiar23@gmail.com>
2019-05-22 20:11:16 -0400

    Minor changes

--- src/views/Reporte.vue
+++ src/views/Reporte.vue
@@ -2,15 +2,21 @@
     <div>
         <h1>Esta es la vista de reporte</h1>
         <div>
-          <ul id="example-1">
-            <li v-bind:key="item" v-for="item in ingresosEgresos">
+          <ul id="reporte">
+            <li v-bind:key="item.ntrans" v-for="item in nuevaList">
               {{ item.ntrans }}
               {{ item.descripcion }}
               {{ item.monto }}
               {{ item.fecha }}
               {{ item.categoria }}
             </li>
-            </ul>
+          </ul>
+        </div>
+        <div>
+        <select @change="printSelectedCategory()" v-model="selectedCategory">
+            <option :key="index" v-bind:value="categoria.nombre" 
+            v-for="(categoria, index) in todasLasCategorias">{{categoria.nombre}}</option>
+          </select>
         </div>
     </div>
 </template>
@@ -22,6 +28,19 @@ export default {
   components: {
 
   },
+  data() {
+    return {
+      categoriaSeleccionada: null,
+      selectedCategory: '',
+      nuevaList: []
+    }
+    
+  },
+  mounted() {
+    var listaUnica = [].concat.apply([], this.ingresosCuentaActual)
+    var listaUnica = listaUnica.concat.apply(listaUnica,this.egresosCuentaActual)
+    this.nuevaList = listaUnica
+  },
   computed:{
     cuentaActual(){
       return this.$store.state.CUENTA_ACTUAL
@@ -32,14 +51,28 @@ export default {
     egresosCuentaActual(){
       return this.$store.state.CUENTAS.filter(cuenta => cuenta.id === this.cuentaActual)[0].egresos
     },
-    ingresosEgresos(){
-      var listaUnica = [].concat.apply([], this.ingresosCuentaActual)
-      var listaUnica = listaUnica.concat.apply(listaUnica,this.egresosCuentaActual)
-      return listaUnica
+    categoriasIngresos(){
+      return this.$store.state.CATEGORIAS_INGRESOS
+    },
+    categoriasEgresos(){
+      return this.$store.state.CATEGORIAS_EGRESOS
+    },
+    todasLasCategorias(){
+      var listaCategorias = [].concat.apply([],[this.categoriasIngresos])
+      var listaCategorias = listaCategorias.concat.apply(listaCategorias,[this.categoriasEgresos])
+      return listaCategorias
     }
   },
   methods:
-  { 
+  {
+    cadenaAFecha(fecha){
+      var tipoFecha= new Date(fecha)
+      return tipoFecha
+    },
+    printSelectedCategory() {
+      this.nuevaList = [];
+      console.log('------' + this.selectedCategory)
+    }
   }
 }
 </script>
